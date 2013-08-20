9f776401e052bf3d9cad206d0364f3da13511452
新工厂 <citoma@zhangchunfengdeMacBook-Air.local>
2013-08-20 16:33:22 +0800

    Initial Commit

---
+++ ZuoxinApp/Zuoxin_comAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Zuoxin_comAppDelegate.h
+//  ZuoxinApp
+//
+//  Created by 新工厂 on 13-8-20.
+//  Copyright (c) 2013年 Zuoxin.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Zuoxin_comAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZuoxinApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZuoxinApp
+//
+//  Created by 新工厂 on 13-8-20.
+//  Copyright (c) 2013年 Zuoxin.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Zuoxin_comAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Zuoxin_comAppDelegate class]));
+    }
+}
